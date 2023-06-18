dc5dbdb766f8ec4dd93449acc76e39a84269139e
milleracero <mchicaa@unal.edu.co>
2023-06-18 15:06:30 -0500

    update weapons System

--- Dungeon Generator/Assets/_Scripts/Weapons/GunController.cs
+++ Dungeon Generator/Assets/_Scripts/Weapons/GunController.cs
@@ -7,13 +7,22 @@ public class GunController : MonoBehaviour
     [SerializeField]
     private Transform WeaponHold;
     [SerializeField]
-    private Gun startingGun;
+    private Gun[] allGuns;
     public Gun equippedGun;
     
     void Start(){
-        if (startingGun != null){
-            EquipGun(startingGun);
+        if (allGuns.Length > 0){
+            EquipGun(0);
         }
+    }
+
+    public void EquipGun(int gunIndex){
+        if (equippedGun != null) {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(allGuns[gunIndex],WeaponHold.position,WeaponHold.rotation) as Gun;
+        equippedGun.transform.parent =WeaponHold;
+
     }
 
     public void EquipGun(Gun gunToEquip){
