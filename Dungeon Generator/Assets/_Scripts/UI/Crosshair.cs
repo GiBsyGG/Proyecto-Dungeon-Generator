11c569565073e5725dcd1754f724986e9b2ee799
GiBsyGG <jcval2801@gmail.com>
2023-06-18 19:29:03 -0500

    Add: Crosshair
    - Se añadió un punto de mira con su correspondiente Script

---
+++ Dungeon Generator/Assets/_Scripts/UI/Crosshair.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Apagamos el cursor normal para poner el nuestro
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mouseCursorPos;
+    }
+}
